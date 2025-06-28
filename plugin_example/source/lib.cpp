c04390d95905f3abe7d6f57ac4f735917c6c38a6
illusiony <37698908+illusion0001@users.noreply.github.com>
2025-06-28 14:33:42 +1200

    Move CXX example out of loader

--- plugin_example/source/lib.cpp
+++ plugin_example/source/lib.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-#include <sstream>
+#include <iostream>
 
 #include <orbis/libkernel.h>
 
@@ -15,10 +15,18 @@ attr_public uint32_t g_pluginVersion = 0x00000100;  // 1.00
 extern "C" int plugin_load(int* argc, const char** argv)
 {
     // https://github.com/OpenOrbis/OpenOrbis-PS4-Toolchain/blob/63c0be5ffff09fbaebebc6b9a738d150e2da0205/samples/library_example/library_example/lib.cpp
-    std::ostringstream os;
-    os << "Hi I'm from the library!\n"
-       << "You passed: " << *argc << " args\n"
-       << "Built: " << __TIME__ << " " << __DATE__;
-    Notify(TEX_ICON_SYSTEM, os.str().c_str());
+    // Just a copy of example for now
+    char str[128] = {0};
+    snprintf(str, sizeof(str),
+             "Hi I'm from the library!\n"
+             "You passed: %d args\n"
+             "Built: " __TIME__ " " __DATE__,
+             argc ? *argc : -1);
+    std::string stdstr = str;
+    printf("%s\n", str);
+    printf("std::string %s\n", stdstr.c_str());
+    sceKernelDebugOutText(0, str);
+    sceKernelDebugOutText(0, "\r\n");
+    Notify(TEX_ICON_SYSTEM, stdstr.c_str());
     return 0;
 }
--- plugin_loader/source/lib.cpp
+++ plugin_loader/source/lib.cpp
@@ -1,11 +1,5 @@
-#include <stdio.h>
-#include <string.h>
-#include <sstream>
-
-#include <orbis/libkernel.h>
-
 #include "../../common/plugin_common.h"
-#include "../../common/notify.h"
+#include <stdint.h>
 
 attr_public const char* g_pluginName = "plugin_loader";
 attr_public const char* g_pluginDesc = "Plugin loader.";
@@ -14,14 +8,5 @@ attr_public uint32_t g_pluginVersion = 0x00000100;  // 1.00
 
 extern "C" int plugin_load(int* argc, const char** argv)
 {
-    // https://github.com/OpenOrbis/OpenOrbis-PS4-Toolchain/blob/63c0be5ffff09fbaebebc6b9a738d150e2da0205/samples/library_example/library_example/lib.cpp
-    // Just a copy of example for now
-    std::ostringstream os;
-    os << "Hi I'm from the library!\n"
-       << "You passed: " << *argc << " args\n"
-       << "Built: " << __TIME__ << " " << __DATE__;
-    printf("%s\n", os.str().c_str());
-    return 0;
-    Notify(TEX_ICON_SYSTEM, os.str().c_str());
     return 0;
 }
