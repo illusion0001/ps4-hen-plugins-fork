7dfb41eda3e713e54f3a2929b1280f1757d771a4
illusiony <37698908+illusion0001@users.noreply.github.com>
2025-06-30 20:50:55 +1200

    ftpsrv: CDUP ensure cwd starts with /
    https://github.com/ps5-payload-dev/ftpsrv/commit/ff2ffb5eb99904ee46b8c6e2d4e11cf23a33739c

--- plugin_server/source/ftp/cmd.c
+++ plugin_server/source/ftp/cmd.c
@@ -327,6 +327,11 @@ int ftp_cmd_CDUP(ftp_env_t* env, const char* arg)
 
     if (pos > 0)
     {
+        env->cwd[0] = '/';
+        env->cwd[1] = '\0';
+    }
+    else if (pos > 0)
+    {
         env->cwd[pos] = '\0';
     }
 
