6187fdc894c701c28903d85b6999c37c141444f9
illusiony <37698908+illusion0001@users.noreply.github.com>
2025-07-01 05:27:35 +1200

    Enable `-fvisibility=hidden` and only export needed symbols

--- plugin_example/source/lib.cpp
+++ plugin_example/source/lib.cpp
@@ -14,7 +14,7 @@ attr_public const char* g_pluginDesc = "Demonstrate usage of CXX in module. Base
 attr_public const char* g_pluginAuth = "illusiony";
 attr_public uint32_t g_pluginVersion = 0x00000100;  // 1.00
 
-extern "C" int plugin_load(SceEntry* args, const void* atexit_handler)
+extern "C" attr_public int plugin_load(SceEntry* args, const void* atexit_handler)
 {
     // https://github.com/OpenOrbis/OpenOrbis-PS4-Toolchain/blob/63c0be5ffff09fbaebebc6b9a738d150e2da0205/samples/library_example/library_example/lib.cpp
     // Just a copy of example for now
