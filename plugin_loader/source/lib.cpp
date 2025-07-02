be4cb99102d06a64ea1f0ed35e802e90c9d43ae2
illusiony <37698908+illusion0001@users.noreply.github.com>
2025-07-02 20:59:59 +1200

    Update Git Ver

--- plugin_loader/source/lib.cpp
+++ plugin_loader/source/lib.cpp
@@ -8,7 +8,13 @@ extern "C"
     attr_public const char* g_pluginName = "plugin_loader";
     attr_public const char* g_pluginDesc = "Plugin loader.";
     attr_public const char* g_pluginAuth = "illusiony";
-    attr_public uint32_t g_pluginVersion = 0x00000100;  // 1.00
+    attr_public const char* g_pluginVersion = "Git Commit: " GIT_COMMIT
+                                              "\n"
+                                              "Git Branch: " GIT_VER
+                                              "\n"
+                                              "Git Commit Number: " GIT_NUM_STR
+                                              "\n"
+                                              "Built: " BUILD_DATE;  // 1.00
     attr_public int plugin_load(SceEntry* args, const void* atexit_handler)
     {
         return 0;
