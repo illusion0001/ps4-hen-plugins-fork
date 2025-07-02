4bd8de84f235bfa34560b0dfbd8b570806a25ec0
illusiony <37698908+illusion0001@users.noreply.github.com>
2025-07-02 20:55:07 +1200

    Update paths

--- plugin_mono/source/shellui_patch/settings_menu.h
+++ plugin_mono/source/shellui_patch/settings_menu.h
@@ -3,11 +3,7 @@
 #include <stdint.h>
 #include <orbis/libkernel.h>
 
-#define BASE_PATH "/data/hen"
-#define SHELLUI_DATA_PATH BASE_PATH "/shellui_data"
-#define HEN_INI BASE_PATH "/hen.ini"
-#define HEN_SECTION "HEN"
-#define SHELLUI_HEN_SETTINGS SHELLUI_DATA_PATH "/hen_settings.xml"
+#include "../../../common/path.h"
 
 void UploadOnBranch(void* app_exe);
 void UploadNewCorelibStreamReader(void);
