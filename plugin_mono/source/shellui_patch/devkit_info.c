53efbec665ff53a52c982c21c4bfb438bb183a9a
illusiony <37698908+illusion0001@users.noreply.github.com>
2025-07-05 19:47:04 +1200

    make icon patch more readable

--- plugin_mono/source/shellui_patch/devkit_info.c
+++ plugin_mono/source/shellui_patch/devkit_info.c
@@ -137,11 +137,32 @@ void UploadEventProxyCall(const struct OrbisKernelModuleInfo* info)
     PatchCall(info, "45 85 e4 78 ? 48 8d 7d ? e8 ? ? ? ? 4c 89 f7 48 89 c6", 9, (uintptr_t)EventProxyCStr, 0);
 }
 
+static void KillLocalMonoString(const int pid, const void* start_addr, const size_t start_size, const wchar_t* str, const size_t len)
+{
+    struct MonoStrLocal
+    {
+        uint16_t len_le;
+        wchar_t str[256];
+    };
+    struct MonoStrLocal d = {0};
+    d.len_le = __builtin_bswap16(len);
+    memcpy(&d.str, str, len);
+    const size_t ll = sizeof(uint16_t) + (len - 1) + 1;
+    hex_dump((uintptr_t)&d, ll, 0);
+    const void* icon = Mem_Scan(start_addr, start_size, &d, ll);
+    if (icon)
+    {
+        const uint16_t str_len_le = __builtin_bswap16(*(uint16_t*)icon);
+        const uint16_t str_len_le1 = str_len_le - sizeof(wchar_t);
+        const uint16_t str_len = __builtin_bswap16(str_len_le1);
+        final_printf("str_len_le1 %d\n", str_len_le1);
+        sys_proc_rw(pid, (uintptr_t)icon, &str_len, sizeof(str_len), 1);
+    }
+}
+
 void UploadRegStrCall(const struct OrbisKernelModuleInfo* info, const struct OrbisKernelModuleInfo* app_exe)
 {
     PatchCall(info, "44 89 ff 48 89 c6 4c 89 e2 48 89 c3 c6 00 00 e8 ? ? ? ? 41 89 c7 85 c0 74 ? c6 03 00", 15, (uintptr_t)RegMgrStr, 0);
-    // Not working for < 5.50 yet
-    return;
     const int pid = getpid();
     const void* start_addr = app_exe->segmentInfo[0].address;
     const uintptr_t start_addr2 = (uintptr_t)start_addr;
@@ -155,20 +176,43 @@ void UploadRegStrCall(const struct OrbisKernelModuleInfo* info, const struct Orb
         // clang-format on
         sys_proc_rw(pid, system_name_str, str, sizeof(str), 1);
     }
-    static const char* icons[] = {
-        "00 1B 6D 00 5F 00 66 00 72 00 69 00 65 00 6E 00 64 00 50 00 61 00 6E 00 65 00 6C 00",                    // m_friendPanel
-        "00 21 6D 00 5F 00 65 00 76 00 65 00 6E 00 74 00 42 00 61 00 73 00 65 00 50 00 61 00 6E 00 65 00 6C 00",  // m_eventBasePanel
-        "00 17 6D 00 5F 00 6D 00 61 00 69 00 6C 00 50 00 61 00 6E 00 65 00 6C 00",                                // m_mailPanel
-        "00 21 6D 00 5F 00 70 00 61 00 72 00 74 00 79 00 42 00 61 00 73 00 65 00 50 00 61 00 6E 00 65 00 6C 00",  // m_partyBasePanel
+    struct wstr_data
+    {
+        const wchar_t* s;
+        const size_t l;
     };
+#define i0 "m_friendPanel"
+#define i1 "m_eventBasePanel"
+#define i2 "m_communityPanel"
+#define i3 "m_mailPanel"
+#define i4 "m_partyBasePanel"
+#define my_wchar_len(ii) ((_countof(ii) * sizeof(wchar_t)) - 1)
+    static const struct wstr_data icons[] = {
+        {L"" i0, my_wchar_len(i0)},
+        {L"" i1, my_wchar_len(i1)},
+        {L"" i2, my_wchar_len(i2)},
+        {L"" i3, my_wchar_len(i3)},
+        {L"" i4, my_wchar_len(i4)},
+    };
+#undef i0
+#undef i1
+#undef i2
+#undef i3
+#undef i4
+#undef my_wchar_len
     for (size_t i = 0; i < _countof(icons); i++)
     {
-        const uintptr_t icon = PatternScan(start_addr, start_size, icons[i], 0);
+        KillLocalMonoString(pid, start_addr, start_size, icons[i].s, icons[i].l);
+    }
+    return;
+    for (size_t i = 2; i < 7; i++)
+    {
+        char nicon[256] = {0};
+        snprintf(nicon, _countof(nicon), "ba %02lx 00 00 00 e8 ? ? ? ? 48 8b bd b0 fe ff ff", i);
+        const uintptr_t icon = PatternScan(start_addr, start_size, nicon, 5);
         if (icon)
         {
-            const uint16_t str_len_le = __builtin_bswap16(*(uint16_t*)icon);
-            const uint16_t str_len = __builtin_bswap16(str_len_le - 1);
-            sys_proc_rw(pid, icon, &str_len, sizeof(str_len), 1);
+            sys_proc_memset(pid, icon, 0x90, 5);
         }
     }
     // TODO: Port to other firmwares
