9bd5f2c8bd2b841c87157717f7f8fd1d45e39595
illusiony <37698908+illusion0001@users.noreply.github.com>
2025-12-15 11:54:48 +0000

    Upload `app_info` to plugins

--- plugin_loader/source/lib.cpp
+++ plugin_loader/source/lib.cpp
@@ -29,21 +29,21 @@ static bool simple_get_bool(const char* val)
            startsWithCase(val, "true");
 }
 
-static void load_module(const char* path, SceEntry* args)
+static void load_module(const char* path, SceEntry* args, disk_appinfo* info)
 {
     const int m = sceKernelLoadStartModule(path, 0, 0, 0, 0, 0);
     final_printf("load res 0x%08x for %s\n", m, path);
     if (m > 0)
     {
-        int32_t (*load)(struct SceEntry*) = NULL;
+        int32_t (*load)(struct SceEntry*, disk_appinfo* info) = NULL;
         sceKernelDlsym(m, "plugin_load", (void**)&load);
-        int32_t (*unload)(struct SceEntry*) = NULL;
+        int32_t (*unload)(struct SceEntry*, disk_appinfo* info) = NULL;
         sceKernelDlsym(m, "plugin_unload", (void**)&unload);
         if (load)
         {
-            if (load(args) && unload)
+            if (load(args, info) && unload)
             {
-                unload(args);
+                unload(args, info);
                 const int unload = sceKernelStopUnloadModule(m, 0, 0, 0, 0, 0);
                 final_printf("Unload result 0x%08x\n", unload);
             }
@@ -98,7 +98,7 @@ static void loadPlugins(SceEntry* args)
             {
                 if (simple_get_bool(key->value))
                 {
-                    load_module(key->key, args);
+                    load_module(key->key, args, &info);
                 }
                 key = key->next;
             }
