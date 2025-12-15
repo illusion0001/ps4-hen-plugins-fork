df7b9c05085083736e769c9e2002be549c659965
illusiony <37698908+illusion0001@users.noreply.github.com>
2025-12-15 16:37:47 +0000

    Build: Fix script continuing on failure

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ bash int3.sh 4096 > common/cave.inc.c
 for dr in plugin_*/ ; do
     if [ -d "$dr" ]; then
         echo "::group::Build $dr"
-        make -C "$dr" clean all
+        make -C "$dr" clean all || exit 1
         echo "::endgroup::"
     fi
 done
